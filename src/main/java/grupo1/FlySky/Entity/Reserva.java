c2a95d944a83a4bc02e329a7802f82a7a9b3b64f
Franco Maidana <frandjev@gmail.com>
2023-12-04 21:18:08 -0300

    Se agrega entity

---
+++ src/main/java/grupo1/FlySky/Entity/Reserva.java
@@ -0,0 +1,38 @@
+package grupo1.FlySky.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "RESERVA")
+public class Reserva {
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "reservaSeq")
+    @SequenceGenerator(sequenceName = "reservaSeq", allocationSize = 1, name = "reservaSeq")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ID_CLIENTE")
+    private Cliente clienteId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ID_VUELO")
+    private Vuelo vueloId;
+
+    @Column(name = "CANTIDAD_ASIENTOS")
+    private Integer cantidadAsientos;
+
+
+}
